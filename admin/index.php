5096ede0e6ef902a1187575afb2b442ef8e776c7
Sofia Robb <sofia2341@gmail.com>
2025-10-26 02:38:27 +0000

    updated formating and added icons

--- admin/index.php
+++ admin/index.php
@@ -5,13 +5,43 @@ include_once 'admin_header.php';
 include_once '../header.php';
 ?>
 
-<div class="container">
-  <h2>Admin Tools</h2>
-  <ul>
-    <li><a href="createUser.php">Create User</a></li>
-    <li><a href="manage_groups.php">Manage Groups</a></li>
-  </ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Admin Tools</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+  <h2><i class="fa fa-tools"></i> Admin Tools</h2>
+  
+  <div class="row mt-4">
+    <div class="col-md-6 mb-3">
+      <div class="card h-100">
+        <div class="card-body">
+          <h5 class="card-title"><i class="fa fa-user-plus"></i> Create User</h5>
+          <p class="card-text">Add new users to the system and manage user accounts.</p>
+          <a href="createUser.php" class="btn btn-primary">Go to Create User</a>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-md-6 mb-3">
+      <div class="card h-100">
+        <div class="card-body">
+          <h5 class="card-title"><i class="fa fa-layer-group"></i> Manage Groups</h5>
+          <p class="card-text">Configure organism assembly groups and group descriptions.</p>
+          <a href="manage_groups.php" class="btn btn-primary">Go to Manage Groups</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+</body>
+</html>
 
 <?php
 include_once '../footer.php';
