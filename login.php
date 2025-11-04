a07860387ddc0d7c992069b758cbc63a783dd169
Sofia Robb <sofia2341@gmail.com>
2025-11-04 19:28:34 +0000

    updated bootstrap

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
   <meta charset="UTF-8">
   <title>Login</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container py-5">
 
