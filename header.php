31971228419a9c3a1742a7dde7aae3936a420321
Sofia Robb <sofia2341@gmail.com>
2025-11-07 20:27:11 +0000

    removed style sheet incldue

--- header.php
+++ header.php
@@ -17,7 +17,6 @@ include_once __DIR__ . '/access_control.php';
 
     <!-- Bootstrap 5.3.2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/<?= $site ?>/css/easy_gdb.css">
     <link rel="stylesheet" href="/<?= $site ?>/css/loading_datatable.css">
     <!-- <link rel="stylesheet" href="/<?= $site ?>/css/tree.css"> DISABLED: Conflicts with new bash-style tree -->
 
