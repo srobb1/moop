f2951ef7ec1c1e03e784ce0fed6096ce173914e1
Sofia Robb <sofia2341@gmail.com>
2025-12-05 15:51:02 +0000

    Fix manage_users: add color application code to JS module

--- js/modules/manage-users.js
+++ js/modules/manage-users.js
@@ -3,6 +3,18 @@
  */
 
 document.addEventListener('DOMContentLoaded', function() {
+  // Pre-assign colors to existing organisms (color function defined in inline_scripts)
+  Object.keys(allOrganisms).forEach(org => getColorForOrganism(org));
+  
+  // Apply colors to existing chips
+  document.querySelectorAll('.tag-chip').forEach(chip => {
+    const organism = chip.getAttribute('data-organism');
+    if (organism) {
+      chip.style.background = getColorForOrganism(organism);
+      chip.style.borderColor = getColorForOrganism(organism);
+    }
+  });
+  
   // Setup DataTables if present
   const usersTables = document.querySelectorAll('.users-table');
   usersTables.forEach(table => {
